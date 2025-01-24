c8868678c1694de96d68fafd8ced603160c1b6ce
Danilozxz <progdanilo@gmail.com>
2025-01-24 04:12:04 -0300

    feat: add amigos e exibir lista de amigos

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
+let amigos = [];
+let list = selecionarElemento("#listaAmigos");
 
+function adicionarAmigo() {
+  let campoNome = selecionarElemento("#amigo").value;
 
+  if (campoNome.trim() !== "") {
+    amigos.push(campoNome);
+    exibirAmigos();
+  } else {
+    alert("O campo não pode estar vazio!");
+  }
+}
+
+function exibirAmigos() {
+  list.innerHTML = "";
+  
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+    item.textContent = amigos[i];
+    list.appendChild(item);
+  }
+}
+
+function selecionarElemento(elemento) {
+  return document.querySelector(elemento);
+}
