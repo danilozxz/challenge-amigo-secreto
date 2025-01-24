11f1dcfbaf0f3ee3f810fadc4f846e28b519c269
Danilozxz <progdanilo@gmail.com>
2025-01-24 04:50:24 -0300

    feat: add função de sortear amigo e alertas

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ function adicionarAmigo() {
   if (campoNome.trim() !== "") {
     amigos.push(campoNome);
     exibirAmigos();
+    alerta.classList.add("remove-alerta");
   } else {
-    alert("O campo não pode estar vazio!");
+    let alerta = selecionarElemento("#alerta");
+    alerta.classList.remove("remove-alerta");
   }
+
+  selecionarElemento("#amigo").value = "";
 }
 
 function exibirAmigos() {
   list.innerHTML = "";
-  
+
   for (let i = 0; i < amigos.length; i++) {
     let item = document.createElement("li");
     item.textContent = amigos[i];
@@ -24,4 +28,15 @@ function exibirAmigos() {
 
 function selecionarElemento(elemento) {
   return document.querySelector(elemento);
+}
+
+function sortearAmigo() {
+
+  if (amigos.length < 2) {
+      alert('Para sortear um amigo é necessário ter ao menos 2 amigos na lista.')
+    } else {
+      let resultado = selecionarElemento("#resultado");
+      const amigoSorteado = Math.floor(Math.random() * amigos.length);
+      resultado.innerHTML =  "O amigo secreto sorteado é: " + amigos[amigoSorteado];
+  }
 }
